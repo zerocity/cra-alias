0fa740d73fa87b5598fb2f050c7c9a43785b3eb0
zerocity <pb@zerocity.sh>
2021-12-03 13:23:17 +0100

    Add config for cosmos and jest

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,22 @@
-// config-overrides.js
 const { alias, configPaths, aliasJest } = require("react-app-rewire-alias");
+const aliasMap = configPaths("./tsconfig.paths.json");
 
-const aliasMap = configPaths("./tsconfig.paths.json"); // or jsconfig.paths.json
+const withAlias = alias(aliasMap);
 
-module.exports = alias(aliasMap);
+module.exports = function override(setEnvironment) {
+  let config = setEnvironment(process.env.NODE_ENV);
+  config = withAlias(config);
+  return config;
+};
+
+//
+// with jest alias works a bit diffrent
+//
+//  "@component/*": ["./component/*"],
+//
+//
+// Dont work
+// "@component": ["./component"],
+// import {Foo} from "@component"
+// dont work
 module.exports.jest = aliasJest(aliasMap);
